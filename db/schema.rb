d53cab98c9be99edbdd9e8e0d69aa9ccf55d1537
builtforsomethingbigger <builtforsomethingbigger@gmail.com>
2020-05-05 15:15:49 -0400

    users full crud

--- db/schema.rb
+++ db/schema.rb
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2020_05_04_190630) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password"
+    t.string "email"
     t.string "first_name"
     t.string "last_name"
     t.string "title"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 User.destroy_all
 
-User.create(username: "scases", password: "password", first_name: "Stephen", last_name: "Cases", title: "admin", country: "US", bio: "Biomedical Engineer turned Software Engineer", profile_img: Faker::Placeholdit.image)
-User.create(username: "gsahagian", password: "password", first_name: "Garen", last_name: "Sahagian", title: "admin", country: "US", bio: "INSERT_GAREN_BIO", profile_img: Faker::Placeholdit.image)
-User.create(username: "usherpa", password: "password", first_name: "Urgen", last_name: "Sherpa", title: "admin", country: "US", bio: "INSERT_URGEN_BIO", profile_img: Faker::Placeholdit.image)
+User.create(username: "scases", password: "password", email: "builtforsomethingbigger@gmail.com", first_name: "Stephen", last_name: "Cases", title: "admin", country: "US", bio: "Biomedical Engineer turned Software Engineer", profile_img: Faker::Placeholdit.image)
+User.create(username: "gsahagian", password: "password", email: "g.s.sahagian@gmail.com", first_name: "Garen", last_name: "Sahagian", title: "admin", country: "US", bio: "INSERT_GAREN_BIO", profile_img: Faker::Placeholdit.image)
+User.create(username: "usherpa", password: "password", email: "urgen.nyc@gmail.com", first_name: "Urgen", last_name: "Sherpa", title: "admin", country: "US", bio: "INSERT_URGEN_BIO", profile_img: Faker::Placeholdit.image)
 
 20.times do
     User.create(username: Faker::Internet.username, password: "guest", first_name: Faker::Name.first_name, last_name: Faker::Name.last_name , title: "enthusiast", country: Faker::Address.country_code, bio: Faker::GreekPhilosophers.quote, profile_img: Faker::Placeholdit.image)
