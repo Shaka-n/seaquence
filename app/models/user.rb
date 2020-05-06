18660759e4f0f7cd302e7eb331b170f7ae97278e
builtforsomethingbigger <builtforsomethingbigger@gmail.com>
2020-05-06 18:55:52 -0400

    20200506_Users_model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ApplicationRecord
     has_many :reports
     has_many :follows
     has_many :locations, through: :follows
+
+
+    def self.search(search)
+        if search
+            username = User.where(username: search)
+        else
+        end 
+    end
 end
