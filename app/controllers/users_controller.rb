e01aff57037c224ee1c7b2294e50e600070bb1b2
Garen Sahagian <g.s.sahagian@gmail.com>
2020-05-08 12:33:04 -0400

    authauth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    skip_before_action :authenticate_user, only: [:new, :create, :index]
     before_action :find_user, only: [:show, :update, :destroy]
 
     def index
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,10 @@ Rails.application.routes.draw do
     resources :reports 
   end 
 
+  patch "/sessions/reset", to: "sessions#reset_counter", as: "reset_session"
+  delete "/sessions/logout", to: "sessions#destroy", as: "log_out_session"
+
+  get "/sessions/login", to: "sessions#new", as: "new_login"
+  post "/sessions/login", to: "sessions#create"
+  
 end
