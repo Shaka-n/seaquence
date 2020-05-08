c275dacabb671c848d1ccc6e7b4f2c4e8ab5c0f1
Garen Sahagian <g.s.sahagian@gmail.com>
2020-05-08 12:49:05 -0400

    FINISHED AUTH

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,11 +15,12 @@ class UsersController < ApplicationController
 
     def create
         user = User.create(user_params)
+        # byebug
         if user.valid?
             session[:user_id] = user.id
             redirect_to user_path(user)
         else
-            flash[:errors] = user.errros.full_messages
+            flash[:errors] = user.errors.full_messages
             redirect_to new_user_path 
         end 
     end
@@ -47,7 +48,7 @@ class UsersController < ApplicationController
 
     private
     def user_params
-        params.require(:user).permit(:password_digest, :first_name, :last_name, :title, :country, :bio, :profile_img, :search)
+        params.require(:user).permit(:password, :email, :username, :first_name, :last_name, :title, :country, :bio, :profile_img, :search)
     end
 
     def find_user
